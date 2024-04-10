f3f349554d0251ebe3c9e8068bcee18bd9968b75
Carlos Salguero <carlossalguero441@gmail.com>
2024-04-10 17:27:34 -0600

    docs(roleRoutes): added swagger documentation

--- src/api/routes/role.routes.ts
+++ src/api/routes/role.routes.ts
@@ -3,6 +3,15 @@ import { RoleController } from '../controllers/role.controller';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /roles/create:
+ *   post:
+ *     summary: Creates a new role
+ *     tags:
+ *       - Roles
+ *     description: Verifies the new role ID to ensure it is unique, then creates the role in the database with the provided payload.
+ */
 router.post('/create', RoleController.createRole);
 
 export const RoleRouter = router;
